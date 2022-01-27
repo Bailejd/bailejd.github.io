fa08fbc6c8d1d9a0646dac465dc9b3d5e75f052d
Bailejd <35389557+Bailejd@users.noreply.github.com>
2022-01-27 14:20:43 -0500

    Create temp data

---
+++ src/data.js
@@ -0,0 +1,64 @@
+export const projects = [
+    {
+        title: "project1",
+        primary_language: "Python",
+        languages: ["Python", "JavaScript"],
+        other: ["Django"],
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui at fugiat dolorum, ab est placeat doloribus nobis velit necessitatibus id corporis ipsa saepe veniam iste earum, harum aut vero esse.",
+        image: "",
+        link: "#",
+        repo: "https://github.com/",
+    },
+    {
+        title: "project2",
+        primary_language: "JavaScript",
+        languages: ["N/A"],
+        other: ["React", "Tailwind"],
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui at fugiat dolorum, ab est placeat doloribus nobis velit necessitatibus id corporis ipsa saepe veniam iste earum, harum aut vero esse.",
+        image: "",
+        link: "#",
+        repo: "https://github.com/",
+    },
+    {
+        title: "project3",
+        primary_language: "JavaScript",
+        languages: ["Javascript"],
+        other: ["React", "Tailwind"],
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui at fugiat dolorum, ab est placeat doloribus nobis velit necessitatibus id corporis ipsa saepe veniam iste earum, harum aut vero esse.",
+        image: "",
+        link: "#",
+        repo: "https://github.com/",
+    },
+];
+
+export const languages = [
+    "Python",
+    "C++",
+    "JavaScript",
+];
+
+export const others = [
+    "ReactJS",
+    "Git",
+    "Django",
+    "ElectronJS",
+];
+
+export const experiences = [
+    {
+        title: "experience1",
+        position: "Software Developer",
+        rel_languages: "C++",
+        rel_skills: ["Boost Test", "CMake", "Git"],
+        date: "01/10/21-08/12/21",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla consectetur voluptatum cumque id aut rerum tempore fuga dolores eligendi exercitationem porro laboriosam, animi, incidunt labore. Modi unde repudiandae quo saepe."
+    },
+    {
+        title: "experience2",
+        position: "Intern",
+        rel_languages: "TypeScript",
+        rel_skills: ["ReactJS", "API", "Git"],
+        date: "01/10/21-08/12/21",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla consectetur voluptatum cumque id aut rerum tempore fuga dolores eligendi exercitationem porro laboriosam, animi, incidunt labore. Modi unde repudiandae quo saepe."
+    },
+];
