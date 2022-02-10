2dfb81fce1e308706cb54c2e793d511ca4339816
Bailejd <35389557+Bailejd@users.noreply.github.com>
2022-02-10 00:06:13 -0500

    Reformat About component

--- src/components/About.js
+++ src/components/About.js
@@ -3,15 +3,13 @@ import React from "react";
 export default function About() {
   return (
     <section id="about" className="scroll-m-10">
-      <div className="container mx-auto px-10 py-20 items-center">
-        <div className="md:12 lg:pr-24 md:pr-16 md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            test test Test
-            <br className="hidden lg:inline-block" />
-             test test test.
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+        <div className="">
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+          About
           </h1>
-          <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, doloremque reiciendis placeat numquam, quia laboriosam aut molestiae vero similique, error dolore exercitationem ex. Eius nemo quisquam odit consectetur, architecto repellendus, quae reiciendis dignissimos incidunt asperiores placeat nihil ut, atque et amet. Dolorum quas aliquid esse reiciendis accusantium doloribus. Maiores alias libero illo! Velit vel nobis voluptatem at nisi expedita aut, distinctio fugit cupiditate recusandae ipsam tenetur. Provident voluptas exercitationem dicta aliquid architecto? Temporibus amet praesentium suscipit atque quod eum laboriosam harum maxime totam accusamus autem dignissimos dolorem quia incidunt minima inventore commodi nihil rem, excepturi dolore? Molestiae quam excepturi adipisci.
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem illo eos incidunt nisi? Obcaecati debitis ad, alias odio, nobis ullam qui sint repudiandae ut deserunt illum numquam earum tenetur, porro sed ex commodi magnam rem consectetur tempora soluta ducimus saepe cupiditate. Enim sint alias mollitia nemo ut fugiat, quaerat assumenda!
           </p>
         </div>
       </div>
