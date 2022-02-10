07732a3920a0cdc784a413914181604ab6d23693
Bailejd <35389557+Bailejd@users.noreply.github.com>
2022-02-10 00:05:17 -0500

    Add commas to listed languages and tech

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -6,9 +6,9 @@ export default function ProjectDetails({ id }) {
     <div id={"project-details" + id}>
       <h1 className="title-font text-2xl font-medium text-white mb-3">{projects[id].title}</h1>
       <h2 className="tracking-widest text-lg title-font font-medium text-green-400 mb-1">{projects[id].primary_language}</h2>
-      <h3 className="title-font text-white">{projects[id].languages.join(' ')}</h3>
-      <h3 className="title-font text-white">{projects[id].other.join(' ')}</h3>
-      <p className="leading-relaxed mb-2 px-10">{projects[id].description}</p>
+      <h3 className="title-font text-white">{projects[id].languages.join(', ')}</h3>
+      <h3 className="title-font text-white">{projects[id].other.join(', ')}</h3>
+      <p className="leading-relaxed mt-1 mb-2 px-10">{projects[id].description}</p>
     </div>
   );
 }
