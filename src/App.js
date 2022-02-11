5decc37c0d4ecd330ddcc926dccea1fa48211c64
Bailejd <35389557+Bailejd@users.noreply.github.com>
2022-02-10 23:10:04 -0500

    Format and remove unused styling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Skills from "./components/Skills";
 
 function App() {
   return (
-    <div className="text-gray-400 bg-gray-900 body-font">
+    <div className="bg-gray-900 text-gray-400">
       <Navbar />
       <About />
       <Projects />
