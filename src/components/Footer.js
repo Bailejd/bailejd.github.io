cef29838adc099c6790483eb0a6a1d8dc17c0411
Bailejd <35389557+Bailejd@users.noreply.github.com>
2022-02-27 00:46:28 -0500

    Add React and Tailwind CSS to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,14 @@ export default function Footer() {
       <div className="container mx-auto py-5 text-center lg:px-40">
         <p>Some filler text for the footer</p>
         <p>
-          Made by{" "}
+          Designed and Developed by{" "}
           <a
             href="https://github.com/Bailejd"
             className="text-white underline hover:text-green-400"
           >
             Bailejd
           </a>
+          {" "} using React and Tailwind CSS
         </p>
       </div>
     </section>
